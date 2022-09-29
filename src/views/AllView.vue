854272b039c8608e42a50aca2a40fcd76d36e441
chenyj1999 <coco868589@gmail.com>
2022-09-30 00:34:25 +0800

    first add

---
+++ src/views/AllView.vue
@@ -0,0 +1,65 @@
+﻿
+<script setup>
+    import axios from 'axios';
+    import { ref, reactive, computed } from 'vue';
+  let apiResult = reactive({
+        data: [{
+            "id": "",
+            "title": ""
+        }]
+  });
+
+    axios
+        .get("http://127.0.0.1:8000")
+        .then((res) => {
+            apiResult.data = res.data
+            console.log(apiResult.data);
+        })
+        .catch(function (error) { // 请求失败处理
+            console.log(error);
+        });
+     /* fetch('./data.json').then(res => {
+        console.log(res);
+        return res.json();
+    }).then(result => {
+        console.log(result);
+        apiResult.data = result
+    })
+    console.log(apiResult.data);*/
+
+
+    /*export default {
+        data() {
+            return {
+                info: {
+                    id: "",
+                    title: ""
+                }
+            }
+        },
+        mounted() {
+           
+        }
+    }*/
+    
+    //.then(response => (this.info = response))
+    </script>
+
+
+<template>
+    {{apiResult.data}}
+
+    <table style="margin-top:20px;">
+        <tr>
+            <th>index</th>
+            <th>id</th>
+            <th>name</th>
+        </tr>
+        <tr v-for="(item,index) in apiResult.data">
+            <td>{{index}}</td>
+            <td>{{item.id}}</td>
+            <td>{{item.title}}</td>
+        </tr>
+    </table>
+</template>
+
---
+++ src/views/CreateView.vue
@@ -0,0 +1,34 @@
+﻿<script setup>
+import TheWelcome from '../components/TheWelcome.vue';
+console.log("createview")
+let msg = 'welcome';
+</script>
+
+<template>
+    <div class="create">
+        <form action="" method="post" enctype="multipart/form-data">
+
+            <div class="field">
+                <label for="">標題</label>
+                <input type="text" name="title" v-model="title">
+            </div>
+            <div class="field">
+                <label for="">內文</label>
+                <textarea name="content" cols="30" rows="10" v-model="content"></textarea>
+            </div>
+            <label for="image" class="block text-sm leading-5 font-medium text-gray-700">
+                圖片上傳
+                <input type="file" name="image">
+            </label>
+
+            <div class="actions">
+                <button type="submit" @click="add">新增文章</button>
+            </div>
+
+        </form>
+    </div>
+</template>
+
+<style>
+
+</style>
